26911c5d982b4952787af94e035954d45c760c5a
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-11 20:53:55 +0600

    practice call back

--- app.js
+++ app.js
@@ -1,26 +1,48 @@
-// callback
+// // callback
 
-const takeOrder = (name, callback) => {
-  console.log(`Take order for ${name} `);
-  callback(name);
+// const takeOrder = (name, callback) => {
+//   console.log(`Take order for ${name} `);
+//   callback(name);
+// };
+
+// const processOrder = (name) => {
+//   console.log(`Processing oder for ${name}`);
+
+//   setTimeout(() => {
+//     console.log(`cooking completed`);
+//     console.log(`order processed for ${name}`);
+//     completeOrder(name);
+//   }, 3000);
+// };
+
+// const completeOrder = (name) => {
+//   console.log(`completed order for ${name} `);
+// };
+
+// takeOrder("Sonatan", (name) => {
+//   processOrder(name, () => {
+//     completeOrder(name);
+//   });
+// });
+
+const takeOrder = (callback) => {
+  console.log("take order");
+  callback();
 };
 
-const processOrder = (name) => {
-  console.log(`Processing oder for ${name}`);
-
-  setTimeout(() => {
-    console.log(`cooking completed`);
-    console.log(`order processed for ${name}`);
-    completeOrder(name);
-  }, 3000);
+const processOrder = (callback) => {
+  console.log("Process order");
+  callback();
 };
 
-const completeOrder = (name) => {
-  console.log(`completed order for ${name} `);
+const completeOrder = () => {
+  console.log("Complete Order");
 };
 
-takeOrder("Sonatan", (name) => {
-  processOrder(name, () => {
-    completeOrder(name);
+takeOrder(function () {
+  processOrder(function () {
+    completeOrder();
   });
 });
+// processOrder();
+// completeOrder();
