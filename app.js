155d92d4e836bc2054d5f61e4383f8466c1104cc
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-11 20:48:30 +0600

    learn javascript callback

---
+++ app.js
@@ -0,0 +1,26 @@
+// callback
+
+const takeOrder = (name, callback) => {
+  console.log(`Take order for ${name} `);
+  callback(name);
+};
+
+const processOrder = (name) => {
+  console.log(`Processing oder for ${name}`);
+
+  setTimeout(() => {
+    console.log(`cooking completed`);
+    console.log(`order processed for ${name}`);
+    completeOrder(name);
+  }, 3000);
+};
+
+const completeOrder = (name) => {
+  console.log(`completed order for ${name} `);
+};
+
+takeOrder("Sonatan", (name) => {
+  processOrder(name, () => {
+    completeOrder(name);
+  });
+});
