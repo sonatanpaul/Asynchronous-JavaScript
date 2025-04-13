9752a83fb5b9c2598966c48de7fa21e3260bf9a8
sonatanpaul <iamsonatanpaul@gmail.com>
2025-04-13 21:55:26 +0600

    learn callback function

--- app.js
+++ app.js
@@ -23,3 +23,59 @@
   mySecond();
   myFirst();
 }
+
+{
+  function display(some) {
+    document.getElementById("demo").innerHTML = some;
+  }
+
+  function myCalculator(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+  }
+
+  let result = myCalculator(5, 5);
+  display(result);
+}
+
+{
+  function display(some) {
+    document.getElementById("demo").innerHTML = some;
+  }
+
+  function myCalculator(num1, num2) {
+    let sum = num1 + num2;
+    display(sum);
+  }
+
+  let result = myCalculator(10, 5);
+  // display(result);
+}
+
+{
+  function display(some) {
+    document.getElementById("demo").innerHTML = some;
+  }
+
+  function myCalculator(num1, num2, callback) {
+    let sum = num1 + num2;
+    callback(sum);
+  }
+
+  myCalculator(10, 20, display);
+  // display(result);
+}
+
+{
+  {
+    function myCalculator(num1, num2, callback) {
+      let sum = num1 + num2;
+      callback(sum);
+    }
+
+    myCalculator(10, 40, (some) => {
+      document.getElementById("demo").innerHTML = some;
+    });
+    // display(result);
+  }
+}
